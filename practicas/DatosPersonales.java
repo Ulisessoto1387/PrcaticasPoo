240348de00a1d82516a14d58fcbe4ceb2f761508
Ulisessoto1387 <ulisessot.1007@gmail.com>
2026-02-17 19:28:07 +0000

    Primer y segungo ejercicio

---
+++ practicas/DatosPersonales.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class DatosPersonales {
+    public static void main (String[] args) {
+        int edad = 0; 
+        float estatura = 0f; 
+        String nombre; 
+        char letrafav; 
+        long segundosvividos = 0;
+
+    Scanner entrada = new Scanner(System.in); 
+    System.out.println("Ingresa tu edad: "); 
+    edad = entrada.nextInt(); 
+    System.out.println("Ingresa tu estatura en metros: "); 
+    estatura = entrada.nextFloat(); 
+    entrada.nextLine(); 
+    System.out.println("Ingresa tu nombre completo: ");
+    nombre = entrada.nextLine(); 
+    System.out.println("Ingresa tu letra fav: "); 
+    letrafav = entrada.nextLine().charAt(0);
+    System.out.println("Segundos que has vivido: "); 
+    segundosvividos = entrada.nextLong();  
+
+    System.out.println("");
+    System.out.println("Hola "+nombre+" "); 
+    System.out.println("Mides:"+estatura+" metros y tu letra favorita es:"+letrafav+" "); 
+    System.out.println("Hasta el momento has vivido:"+segundosvividos+" segundos");
+
+    }
+}
